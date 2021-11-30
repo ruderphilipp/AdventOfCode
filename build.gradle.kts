9a263bcb5868a7a1da46f2fb8fbed3ab8e257ac7
Philipp Riemer <ruderphilipp@gmail.com>
2021-11-30 18:42:01 +0100

    add gradle + gradle settings

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    java
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.assertj:assertj-core:3.20.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
